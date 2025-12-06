58fd5481997b585e162394cdb467c2ad4ba73baa
Elias Johnson <eliasjohnson211@gmail.com>
2025-12-06 15:39:07 -0700

    add: format print statements

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pprint import PrettyPrinter
 from stats import get_num_words, char_count
 
 def get_book_text(filepath):
@@ -9,7 +10,15 @@ character_count = char_count(get_book_text("books/frankenstein.txt"))
 
 def main():
     """main function"""
-    print(get_num_words(get_book_text("books/frankenstein.txt"))) 
-    print(char_count(get_book_text("books/frankenstein.txt")))
+    filepath = "books/frankenstein.txt"
+    print("\n============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath} ")
+    print("----------- Word Count ----------")
+    print(get_num_words(get_book_text(filepath)))
+    print("--------- Character Count -------")
+    print(char_count(get_book_text(filepath)))
+    print("============= END ===============\n")
 
 main()
+
+#case number 21693998
